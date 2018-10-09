8a6d7bd6b0a852faaf3dd44c2d12cf111970f5f1
zhifeng ou <zhifengou@hotmail.com>
2018-10-09 14:01:17 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+  erb :user_input
+end
+
+  post '/piglatinize' do 
+    @user_phrase = params[:user_phrase]
+  erb :piglatinize
+end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,7 @@
+class Piglatinizer
+  attr_accessor: user_phrase 
+  def initialize (user_phrase)
+    @user_phrase = user_phrase
+  end
+  
+end
